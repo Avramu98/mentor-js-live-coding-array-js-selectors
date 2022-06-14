5775ab0b79d4ae13262bd8a5d711c06968931376
Avram Vlad <vlad@Avrams-MacBook-Pro.local>
2022-06-14 20:26:32 +0300

    array

--- index.js
+++ index.js
@@ -1,47 +1,29 @@
-// let name = "Vlad";
-
-// ES5 varianta veche de JS
-// ES6 varianta mai noua
-
-// Primitive data types
-let stringVar = " asdlksdjfjkds ";
-let numberVar = 0.2222;
-let booleanVar = false;
-let undefinedVar;
-let nullVar = null;
-// console.log(typeof stringVar);
-// console.log(typeof numberVar);
-// console.log(typeof booleanVar);
-// console.log(typeof undefinedVar);
-
-// let name = "Vlad";
+let numberOne = 1;
+let numberTwo = 2;
+let numberThree = 3;
+let name = 'Vlad';
+let booleanVar = true;
 
 
+let arrayVar = [numberOne, numberTwo, numberThree, name, booleanVar];
 
-// function testingVariables() {
-//     let name = "Avram";
-//     return name;
+// console.log(arrayVar);
+
+let iterator = 0;
+// iterator = iterator + 1;
+iterator++
+// console.log(iterator);
+
+let fruits = ['apple', 'orange', ' strawberry', 'lemon'];
+// console.log(array[0]);
+// console.log(array.length);
+// for(let i=0; i < array.length; i++) {
+//     console.log(array[i])
+//     console.log(`Iteratia se afla pe i cu valoarea ${i} ${array[i]}`)
 // }
 
-// let returnedNameFromFunction = testingVariables();
+let arrowFunction = () => {}
 
-// document.body.innerHTML =  returnedNameFromFunction;
-
-// console.log(returnedNameFromFunction);
-let weatherType = "sunny";
-
-let whatToWear = (weatherCondition) => {
-    let condition;
-    if(weatherCondition === "raining") {
-        let condition = "ia o geaca";
-        return condition;
-    } else if(weatherCondition === "sunny") {
-        let condition = "ia ochelari de soare"
-        return condition;
-    } else {
-        let condition = "nu stiu ce sa fac"
-        return condition;
-    }
-}
-
-document.body.innerHTML = whatToWear(weatherType);
+fruits.map((fruit) => {
+    console.log(fruit)
+})
